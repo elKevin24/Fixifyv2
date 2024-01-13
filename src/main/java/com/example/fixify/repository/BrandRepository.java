4b8c49b9cd91c14dd523eb2ae30682484ad93726
elKevin24 <36934396+elKevin24@users.noreply.github.com>
2024-01-13 04:39:26 -0600

    Add brand and category controllers, repositories, and services

---
+++ src/main/java/com/example/fixify/repository/BrandRepository.java
@@ -0,0 +1,4 @@
+package com.example.fixify.repository;
+
+public interface BrandRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.fixify.models.catalogsDevice.Brand, java.lang.Long> {
+}
---
+++ src/main/java/com/example/fixify/repository/CategoryRepository.java
@@ -0,0 +1,4 @@
+package com.example.fixify.repository;
+
+public interface CategoryRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.fixify.models.catalogsDevice.Category, java.lang.Long> {
+}
